6589dea0a792a632fb58097c0c6fa826f9703b98
jelee <jelee2204@dgu.ac.kr>
2025-08-01 09:12:07 +0900

    2193 이친수

---
+++ 2193.py
@@ -0,0 +1,16 @@
+#20250801
+#2193 이친수
+#다이나믹 프로그래밍
+
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+d=[[0 for j in range(2)] for i in range(n+1)]
+d[1][1]=1
+d[1][0]=0
+
+for i in range(2,n+1):
+    d[i][0]=d[i-1][1]+d[i-1][0]
+    d[i][1]=d[i-1][0]
+print(d[n][0]+d[n][1])
