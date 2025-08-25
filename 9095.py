32a4927bdc5bd8b364e51829016fe146e992be39
jelee <jelee2204@dgu.ac.kr>
2025-08-25 09:21:12 +0900

    9095 1,2,3 더하기

---
+++ 9095.py
@@ -0,0 +1,17 @@
+#20250825
+#9095 1,2,3 더하기
+#dp
+
+import sys
+input=sys.stdin.readline
+t=int(input())
+dp=[0]*12
+dp[1]=1
+dp[2]=2
+dp[3]=4
+for i in range(4,12):
+    dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+
+for i in range(t):
+    n=int(input())
+    print(dp[n])
