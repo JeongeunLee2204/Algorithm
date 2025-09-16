2bfb23c6ecc9d2f5656d212525cae78a7dd18ca1
jelee <jelee2204@dgu.ac.kr>
2025-09-16 16:57:02 +0900

    16953 A->B

---
+++ 16953.py
@@ -0,0 +1,19 @@
+#20250916
+#16953 A->B
+#top down 구현, 그리디
+
+a, b = map(int, input().split())
+r = 1
+while (b != a):
+    r += 1
+    temp = b
+    if b % 10 == 1:
+        b //= 10
+    elif b % 2 == 0:
+        b //= 2
+
+    if temp == b:
+        print(-1)
+        break
+else:
+    print(r)
