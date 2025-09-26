f7530f26479d257169f99ebe5a9aedbde35cee94
jelee <jelee2204@dgu.ac.kr>
2025-09-26 23:29:06 +0900

    17946 피자는 나눌수록 커지잖아요

---
+++ 17946.py
@@ -0,0 +1,14 @@
+#20250926
+#17946 피자는 나눌 수록 커지잖아요
+
+#규칙을 찾아보니 항상 답은 1...
+import sys
+input=sys.stdin.readline
+n=int(input())
+for _ in range(n):
+    # a=int(input())
+    # give=0
+    # for i in range(1,a+1):
+    #     give+=i
+    # print(2*a-give)
+    print(1)
