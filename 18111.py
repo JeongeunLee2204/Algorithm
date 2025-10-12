cd38472e49c51fc9472d9ecb672201bcbd299313
jelee <jelee2204@dgu.ac.kr>
2025-10-12 20:44:15 +0900

    18111 마인크래프트

--- 18111.py
+++ 18111.py
@@ -1,11 +1,36 @@
 #20250904
 #18111 마인크래프트
+#brute force, histogram
 
-n,m,b=map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-a=[]
-for _ in range(n):
-    tmp=list(map(int,input().split()))
-    a.append(tmp)
+n, m, b = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
 
-print(a)
+height_count = [0] * 257
+for row in a:
+    for h in row:
+        height_count[h] += 1
+
+min_time = float('inf')
+best_height = 0
+
+for i in range(257):  # 목표 높이
+    tmp_b = b
+    tmp_time = 0
+    for h in range(257):
+        diff = h - i
+        if diff > 0:
+            tmp_time += 2 * diff * height_count[h]
+            tmp_b += diff * height_count[h]
+        elif diff < 0:
+            tmp_time += (-diff) * height_count[h]
+            tmp_b -= (-diff) * height_count[h]
+    if tmp_b < 0:
+        continue
+    if tmp_time < min_time or (tmp_time == min_time and i > best_height):
+        min_time = tmp_time
+        best_height = i
+
+print(min_time, best_height)
